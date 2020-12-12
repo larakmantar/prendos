ce61c9d1a119e9471fde32a8ebdf8fbb3ad68301
larakmantar <69216894+larakmantar@users.noreply.github.com>
2020-12-12 09:26:30 +0100

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+module.exports = member => {
+    //member.addRole(member.guild.roles.find(r => r.name.startsWith('Kullanıcılar')));
+    const channel = member.guild.channels.find('name', 'giriş-çıkış');
+    if (!channel) return;
+   const embed = new Discord.RichEmbed()
+   .setColor('RANDOM')
+   .setAuthor(member.user.tag, member.user.avatarURL || member.user.defaultAvatarURL)
+   .setThumbnail(member.user.avatarURL || member.user.defaultAvatarURL)
+   .setTitle('Üye katıldı;')
+   .setDescription(`Sunucuya katıldı [${member.guild.memberCount} üye]!`)
+   .setFooter('BOT İSMİ')
+   .setTimestamp()
+   channel.send(embed);
+};
+
+
+
+
+
+/////////Bu Dosya Adem Can Tarafından Yapılmıştır !/////////////
---
+++ komut taslaßç.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js'); //bu bir modül modüllerin ne işe yaradığını öğrenmek için 'Modül Yükleme' klasörüne
+
+
+exports.run = (client, message) => {
+
+
+};
+
+exports.conf = {
+  enabled: true, //komutun açık kapalı olduğunu gösterir | true = açık false = kapalı
+  guildOnly: false, //komutu herkes kullanabiliyormu onu gösterir
+  aliases: ['1', '2'], //komutun farklı kullanımları
+  permLevel: 0 //kimler kullancağını belirtir 
+};
+
+exports.help = {
+  name: 'komutadı', //adını belirtin (kullanmak için gereken komut) Örneğin otorol NOT Hangi ismi yazarsanız komut o isimle çalışır
+  description: 'komutaçıklaması', //komutun açıklaması
+  usage: 'kullanımı' //komutun kullanım şekli (örneğin !otorol <@rol> <#kanal>)
+};
+
+
+
+
